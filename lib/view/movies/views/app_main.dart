4c74c150210665bb40a138b27dad777ab16f0427
ahmed <Ahmed20Hassan01@gmail.com>
2025-02-19 12:55:10 +0200

    BottomNavigationBar

--- lib/view/movies/views/app_main.dart
+++ lib/view/movies/views/app_main.dart
@@ -17,18 +17,6 @@ class AppMainView extends StatefulWidget {
 
 class _AppMainViewState extends State<AppMainView> {
   int currentIndex = 0;
-  /* Widget _getNavItem(String imageNAme, int index) {
-    return currentIndex == index
-        ? Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 6),
-        decoration: BoxDecoration(
-          color: AppColors.kBlackColor.withOpacity(.6),
-          borderRadius: BorderRadius.circular(66),
-        ),
-        child: ImageIcon(AssetImage('assets/images/home/$imageNAme')))
-        : ImageIcon(AssetImage('assets/images/home/$imageNAme'));
-  }*/
-
   List<Widget> screens = [
     HomeView(),
     SearchView(),
@@ -38,45 +26,62 @@ class _AppMainViewState extends State<AppMainView> {
 
   @override
   Widget build(BuildContext context) {
-
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar:  PreferredSize(
-        preferredSize: Size.fromHeight(0),
-        child: AppBar(
-          systemOverlayStyle: SystemUiOverlayStyle(
-              statusBarColor:AppColors.darkBackground
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(0),
+          child: AppBar(
+            systemOverlayStyle:
+            SystemUiOverlayStyle(statusBarColor: AppColors.darkBackground),
           ),
         ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.transparent,
-    padding: EdgeInsets.only(bottom: 8,left: 8 , right: 8),
-        notchMargin: 0.8,
-        elevation: 10,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(16),
-          child: BottomNavigationBar(
-            backgroundColor:AppColors.grayBackground,
-            selectedItemColor:AppColors.kPrimaryColor,
-            unselectedItemColor: AppColors.textWhiteColor,
-            type: BottomNavigationBarType.fixed,
-            currentIndex: currentIndex,
-            showUnselectedLabels: false,
-            showSelectedLabels: false,
-            onTap: (value) {
-              currentIndex = value;
-              setState(() {});
-            },
-            items: [
-            BottomNavigationBarItem(icon: ImageIcon(AssetImage(KImages.homeIcon)), label: ''),
-            BottomNavigationBarItem(icon: ImageIcon(AssetImage(KImages.searchIcon)) , label: ''),
-            BottomNavigationBarItem(icon: ImageIcon(AssetImage(KImages.exploreIcon)) , label: ''),
-            BottomNavigationBarItem(icon: ImageIcon(AssetImage(KImages.profileIcon)) , label: ''),
+        body: Stack(
+            children: [
+              screens[currentIndex],
+              Positioned(
+                bottom: 8,
+                left: 8,
+                right: 8,
+                child: SizedBox(
+                  width: width,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: SizedBox(
+                      height: 60,
+                      child: BottomNavigationBar(
+                        backgroundColor: AppColors.grayBackground,
+                        selectedItemColor: AppColors.kPrimaryColor,
+                        unselectedItemColor: AppColors.textWhiteColor,
+                        type: BottomNavigationBarType.fixed,
+                        currentIndex: currentIndex,
+                        showUnselectedLabels: false,
+                        showSelectedLabels: false,
+                        onTap: (value) {
+                          currentIndex = value;
+                          setState(() {});
+                        },
+                        items: [
+                          BottomNavigationBarItem(
+                              icon: ImageIcon(AssetImage(KImages.homeIcon)),
+                              label: ''),
+                          BottomNavigationBarItem(
+                              icon: ImageIcon(AssetImage(KImages.searchIcon)),
+                              label: ''),
+                          BottomNavigationBarItem(
+                              icon: ImageIcon(AssetImage(KImages.exploreIcon)),
+                              label: ''),
+                          BottomNavigationBarItem(
+                              icon: ImageIcon(AssetImage(KImages.profileIcon)),
+                              label: ''),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ],
-          ),
-        ),
-      ),
-      body: screens[currentIndex],
-    );
-  }
+            ),
+        );
+    }
 }
