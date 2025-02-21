e5d1b79b712e44a63bf7aa6bb0ba1c81bad4667a
ahmed <Ahmed20Hassan01@gmail.com>
2025-02-21 17:08:17 +0200

    work on profile

--- lib/Features/movies/profile/view/profile_view.dart
+++ lib/Features/movies/profile/view/profile_view.dart
@@ -1,19 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:movies/core/utility/theme_data/custom_theme/text_theme.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Row(
-          children: [
-            Image.asset('assets/images/avatar2.png'),
-          ],
-        )],
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(height: height*.05,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Image.asset('assets/images/avatar2.png'),
+                  SizedBox(height: 16,),
+                  Text('name of user',
+                  style: KStyles.roboto20w700White,)
+                ],
+              ),
+              Column(
+                children: [
+                  Text('12',
+                    style: KStyles.roboto20w700White,),
+                  SizedBox(height: 20,),
+                  Text('Wish List',
+                    style: KStyles.roboto20w700White,),
+                ],
+              ),
+              Column(
+                children: [
+                  Text('10',
+                    style: KStyles.roboto20w700White,),
+                  SizedBox(height: 20,),
+
+                  Text('History',
+                    style: KStyles.roboto20w700White,),
+                ],
+              ),
+
+            ],
+          ),
+
+        ],
+      ),
     );
   }
 }
